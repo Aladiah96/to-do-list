c2d13a4dde3b3a5f1df986ad87937053b7d5c837
Aladiah96 <63356947+Aladiah96@users.noreply.github.com>
2021-12-12 18:53:26 -0300

    Only missing filter

--- script/script.js
+++ script/script.js
@@ -1,15 +1,49 @@
-document.getElementById('add').onclick = function add() {
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
-    var input = document.createElement("INPUT");
-    input.setAttribute("type", "checkbox");
-    const val = document.querySelector('input').value;
-    const ul = document.getElementById('tasks');
-    let li = document.createElement('li');
-    
-    li.appendChild(input);
-    li.appendChild(document.createTextNode(val));
-    ul.appendChild(li);
-    };
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
+function addTodo(event) {
+    event.preventDefault();
 
-    
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class= "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+ 
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class= "fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+};
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
+    }
+
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
